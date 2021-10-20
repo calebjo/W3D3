d5ee99970f0f1e7b42d8c39214e4cb3b051aeba4
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-20 15:27:41 -0400

    Added bsearch method

--- recursion.rb
+++ recursion.rb
@@ -95,7 +95,22 @@ def iter_fib(n)
 end
 
 
-def binary_search
+def bsearch(array, target)
+    midpoint = array.length / 2 
+    return nil if !array.include?(target)
+    return midpoint if target == array[midpoint]
 
-
+    if target < array[midpoint]
+        bsearch(array[0..midpoint-1], target) # first half
+    else
+        midpoint + bsearch(array[midpoint..-1], target) # last half
+    end
 end
+
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
