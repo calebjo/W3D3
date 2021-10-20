6e775857c80b86ce2fee908de7249f9005b40894
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-20 14:07:54 -0400

    Added files for Exponentiation

--- recursion.rb
+++ recursion.rb
@@ -11,11 +11,46 @@ end
 # p rec_range(1, 3)  #[1, 2]
 # p rec_range(1, 5)
 
-def iter_sum(arr)
+def rec_sum(arr)
     return nil if arr.empty? 
     return arr[0] if arr.length == 1
 
-    arr[0] + iter_sum(arr[1..-1])
+    arr[0] + rec_sum(arr[1..-1])
 
 end
 
+def iter_sum(arr)
+    sum = 0
+    arr.each {|num| sum += num }
+    sum
+end
+
+# --------------------------------------------------------------------------------
+# Exponentiation
+
+def rec_expo1(b, n)
+    return 1 if n == 0
+    return b if n == 1
+
+    rec_expo(b, n - 1) * b
+end
+
+def rec_expo2(b, n)
+    return 1 if n == 0
+    return b if n == 1
+
+    if n.even?
+        rec_expo2(b, n / 2) * b
+    else
+        b * (rec_expo2(b, (n - 1) / 2) * b)
+    end
+end
+
+# --------------------------------------------------------------------------------
+# Deep Dup
+
+class Array
+    def deep_dup
+        
+    end
+end
