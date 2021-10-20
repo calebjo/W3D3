d47b872d5a8df39bf7a022d0bc20944386b32bec
Sonja <sonja.ng1@gmail.com>
2021-10-20 13:50:24 -0400

    finished iter_sum

--- recursion.rb
+++ recursion.rb
@@ -1,20 +1,21 @@
 def rec_range(first, last)
-    # return [] if last < first
-    # return [first] if first == last
-
-    # rec_range(first, )
-
-    # # new_arr = []
-    # # new_arr << [rec_range(first, last-1)].concat([last])
-
-    # new_arr
- 
     return [] if last < first
-    return [first] if first == last
+    return [first] if first == last || last - first == first
 
-    new_arr = []
-    new_arr += rec_range(first, last)
+    new_arr = rec_range(first, last - 1) 
+        new_arr << last - first
 end
 
-p rec_range(1, 2)
-p rec_range(1, 5)
+# rec_range(1, 1) #[1] 
+# p rec_range(1, 2)  #[1] << 2
+# p rec_range(1, 3)  #[1, 2]
+# p rec_range(1, 5)
+
+def iter_sum(arr)
+    return nil if arr.empty? 
+    return arr[0] if arr.length == 1
+
+    arr[0] + iter_sum(arr[1..-1])
+
+end
+
